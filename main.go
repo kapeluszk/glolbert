cd0fbe6780aba1d0ffdd9837acc7c46c3b8ccb5e
kacper <126521076+kapeluszk@users.noreply.github.com>
2024-02-19 23:10:57 +0100

    fixed fetching nicknames

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ var (
 )
 
 func main() {
+	flag.Parse()
 	dc, err := discordgo.New("Bot " + *BotToken)
 	if err != nil {
 		log.Fatalf("Invalid bot parameters: %v", err)
@@ -71,18 +72,40 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 					for _, vss := range guild.VoiceStates {
 						if vss.ChannelID == vs.ChannelID {
 							users = append(users, vss.UserID)
+
 						}
 					}
 				}
 			}
-			mid := len(users) / 2
-			team1 := users[:mid]
-			team2 := users[mid:]
+			fmt.Println(users)
+			var nicknames []string
+			members, err := s.GuildMembers(guild.ID, "", 1000)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, member := range members {
+				fmt.Println(member.User.ID)
+			}
+			for _, uid := range users {
+				for _, member := range members {
+					if uid == member.User.ID {
+						nickname := member.Nick
+						if nickname == "" || nickname == "0" {
+							nickname = member.User.Username
+						}
+						nicknames = append(nicknames, nickname)
+					}
+				}
+			}
+			fmt.Println(nicknames)
+			mid := len(nicknames) / 2
+			team1 := nicknames[:mid]
+			team2 := nicknames[mid:]
 
 			res1 := strings.Join(team1, "\n")
 			res2 := strings.Join(team2, "\n")
 
-			result := fmt.Sprintf("Wylosowałem drużyny!\n Drużyna 1: %s\n Drużyna 2: %s", res1, res2)
+			result := fmt.Sprintf("Wylosowałem drużyny!\n Drużyna 1: %s\n Drużyna 2: %s\n", res1, res2)
 
 			_, _ = s.ChannelMessageSend(m.ChannelID, result)
 		}
